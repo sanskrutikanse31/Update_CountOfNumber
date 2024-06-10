b795f11e5a15f8007ee9d35314474d6e6840af88
Sanskruti Kanse <sanskruti.kanse@olamnet.com>
2024-06-10 14:19:31 +0530

    UpdateCountCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+import React, { useState } from "react"; 
+//FunctionalComponent
+const App= () => { 
+	const [count, setCount] = useState(0); 
+
+	const increase = () => { 
+		setCount(count + 1); 
+	} 
+
+	return ( 
+		<div style={{ margin: '50px' }}> 
+			 
+			<h3>Counter App using Functional Component : </h3> 
+			<h2>{count}</h2> 
+			<button onClick={increase}>Add</button> 
+		</div> 
+	) 
+} 
+
+export default App; 
